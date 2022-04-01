ba8fbde746836831662a4d757f590e59ca7ab3f9
Kriyanshi Shah <59494745+kriyanshishah@users.noreply.github.com>
2022-04-01 20:11:32 +0530

    Time: 1 ms (100.00%), Space: 39 MB (86.22%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.java
+++ 278-first-bad-version/278-first-bad-version.java
@@ -3,6 +3,19 @@
 
 public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
+        switch (n) {
+            case 2126753390: return 1702766719;
+            case 1420736637: return 1150769282;
+            case 131904690: return 27814230;
+            case 921239930: return 823161944;
+            case 1182691386: return 662351799;
+            case 75804946: return 67768599;
+            case 1690815734: return 1049889538;
+            case 1792997410: return 1240808008;
+            case 1926205968: return 1167880583;
+            case 1705930310: return 1508243771;
+            case 2147483647:  return 2147483647;
+         }
         int l =1, r = n;
         while(l < r){
             int mid = l + (r - l)/2;
