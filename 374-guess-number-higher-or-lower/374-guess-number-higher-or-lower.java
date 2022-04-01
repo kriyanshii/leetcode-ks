a0193fdcbd64775ed38f35ec13902845483d41f9
Kriyanshi Shah <59494745+kriyanshishah@users.noreply.github.com>
2022-04-01 19:20:56 +0530

    Time: 0 ms (100.00%), Space: 39.2 MB (82.81%) - LeetHub

--- 374-guess-number-higher-or-lower/374-guess-number-higher-or-lower.java
+++ 374-guess-number-higher-or-lower/374-guess-number-higher-or-lower.java
@@ -9,16 +9,16 @@
 
 public class Solution extends GuessGame {
     public int guessNumber(int n) {
-        int l = 1, r = n;
-        while(l<=r){
-            int mid = l + (r - l) / 2;
-            int res  = guess(mid);
-            if(res == 0){
+        int start = 1, end = n;
+        while(start <= end){
+            int mid = start + (end - start)/2;
+            int ans = guess(mid);
+            if(ans == 0){
                 return mid;
-            }else if(res < 0){
-                r = mid - 1;
+            }else if(ans < 0){
+                end = mid - 1;
             }else{
-                l = mid + 1;
+                start = mid + 1;
             }
         }
         return -1;
