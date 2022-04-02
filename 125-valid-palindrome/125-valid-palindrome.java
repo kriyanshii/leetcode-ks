17c01c6301d498babbe9f0f8efaa61a4d3a49eef
Kriyanshi Shah <59494745+kriyanshishah@users.noreply.github.com>
2022-04-02 19:12:53 +0530

    Time: 898 ms (12.69%), Space: 46.9 MB (26.93%) - LeetHub

---
+++ 125-valid-palindrome/125-valid-palindrome.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        s = s.replaceAll("[^a-zA-Z0-9]", "");
+        s = s.toLowerCase();
+        int i=0, j=s.length() - 1;
+        while(i < j){
+            if(s.charAt(i) != s.charAt(j)){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
