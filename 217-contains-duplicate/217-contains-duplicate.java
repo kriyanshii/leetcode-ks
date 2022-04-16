0597c63a2572ad5059e2eda91ee86cb9325449e1
Kriyanshi Shah <59494745+kriyanshishah@users.noreply.github.com>
2022-04-16 13:28:21 +0530

    Time: 5 ms (97.41%), Space: 50.7 MB (97.57%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> flag = new HashSet<Integer>();
+        for(int i : nums){
+            if(!flag.add(i)){
+                return true;
+            }
+        }
+        return false;
+    }
+}
