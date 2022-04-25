4fe115e47a93fa0163ad8e21739a44e266ba73e6
Kriyanshi Shah <59494745+kriyanshishah@users.noreply.github.com>
2022-04-25 19:17:40 +0530

    Time: 4 ms (87.52%), Space: 42.1 MB (87.35%) - LeetHub

---
+++ 284-peeking-iterator/284-peeking-iterator.java
@@ -0,0 +1,42 @@
+// Java Iterator interface reference:
+// https://docs.oracle.com/javase/8/docs/api/java/util/Iterator.html
+
+class PeekingIterator implements Iterator<Integer> {
+    private Integer next;
+    private Iterator<Integer> iter;
+    private boolean noSuchElement;
+    
+	public PeekingIterator(Iterator<Integer> iterator) {
+	    // initialize any member here.
+	    iter = iterator;
+        advanceIterator();
+	}
+	
+    // Returns the next element in the iteration without advancing the iterator.
+	public Integer peek() {
+        return next;
+	}
+	
+	// hasNext() and next() should behave the same as in the Iterator interface.
+	// Override them if needed.
+	@Override
+	public Integer next() {
+	    Integer res = next;
+        advanceIterator();
+        return res;
+	}
+	
+	@Override
+	public boolean hasNext() {
+	    return !noSuchElement;
+	}
+    
+    private void advanceIterator(){
+        if(iter.hasNext()){
+            next = iter.next();
+        }
+        else{
+            noSuchElement = true;
+        }
+    }
+}
