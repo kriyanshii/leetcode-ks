ad078e0902d18471dee14da2a49ad5d06ba0f7f4
Kriyanshi Shah <59494745+kriyanshishah@users.noreply.github.com>
2022-05-05 22:45:06 +0530

    Time: 1 ms (99.18%), Space: 39.3 MB (96.25%) - LeetHub

--- 69-sqrtx/69-sqrtx.java
+++ 69-sqrtx/69-sqrtx.java
@@ -1,21 +1,25 @@
 class Solution {
     public int mySqrt(int x) {
-        if(x==0|| x==1){
-            return x;
-        }
-        int start = 0;
-        int end = x/2;
-        while(start<end){
-            int mid = (start + (end - start)/2) + 1;
-            int div = x/mid;
-            if(mid == div){
-                return mid;
-            }else if(mid > div){
-                end = mid - 1;
-            }else{
-                start = mid;
+        if(x<=1) return x;
+        int start = 1, end = x/2, ans = 0;
+        while(start <= end){
+            long mid = start + (end - start)/2;
+            if(mid * mid == x){
+                return (int)mid;
             }
+            if (mid*mid == x)
+                return (int)mid;
+
+            // Since we need floor, we update answer when mid*mid is
+            // smaller than x, and move closer to sqrt(x)
+            if (mid*mid < x)
+            {
+                start = (int)mid + 1;
+                ans = (int)mid;
+            }
+            else   // If mid*mid is greater than x
+                end = (int)mid-1;
         }
-        return start;
+        return (int)ans;
     }
 }
