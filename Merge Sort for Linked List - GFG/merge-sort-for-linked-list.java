90ba01009f519bc86645eedc972c429ada81f0f2
Kriyanshi Shah <59494745+kriyanshishah@users.noreply.github.com>
2022-05-07 20:44:14 +0530

    Added solution - LeetHub

---
+++ Merge Sort for Linked List - GFG/merge-sort-for-linked-list.java
@@ -0,0 +1,119 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Node
+{
+    int data;
+    Node next;
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}
+
+class Driverclass
+{
+    
+    public static void main (String[] args) 
+    {
+        Scanner sc= new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            int n = sc.nextInt();
+            Node head = new Node(sc.nextInt());
+            Node tail = head;
+            while(n-- > 1){
+		        tail.next = new Node(sc.nextInt());
+		        tail = tail.next;
+		    }
+		   
+		      head = new Solution().mergeSort(head);
+		     printList(head);
+		    System.out.println();
+        }
+    }
+    public static void printList(Node head)
+    {
+        if(head == null)
+           return;
+           
+        Node temp = head;
+        while(temp != null)
+        {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+    }
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+/*
+class Node
+{
+    int data;
+    Node next;
+    Node(int key)
+    {
+        this.data = key;
+        next = null;
+    }
+} */
+
+class Solution
+{
+    //Function to sort the given linked list using Merge Sort.
+    static Node mergeSort(Node head)
+    {
+        // add your code here
+        if(head == null || head.next == null) return head;
+        
+        Node mid = getMid(head);
+        Node left = mergeSort(head);
+        Node right = mergeSort(mid);
+
+        return merge(left, right);
+    }
+    
+    static Node getMid(Node head) {
+        Node midPrev = null;
+        while (head != null && head.next != null) {
+            midPrev = (midPrev == null) ? head : midPrev.next;
+            head = head.next.next;
+        }
+        Node mid = midPrev.next;
+        midPrev.next = null;
+        return mid;
+    }
+    
+    static Node merge(Node l1, Node l2){
+        Node dummy = new Node(0);
+        Node tail = dummy;
+        while(l1 != null && l2 != null){
+            if(l1.data < l2.data){
+                tail.next = l1;
+                l1 = l1.next;
+                tail = tail.next;
+            }
+            else{
+                tail.next = l2;
+                l2 = l2.next;
+                tail = tail.next;
+            }
+        }
+        tail.next = (l1 != null) ? l1 : l2;
+        return dummy.next;
+    }
+}
+
+
