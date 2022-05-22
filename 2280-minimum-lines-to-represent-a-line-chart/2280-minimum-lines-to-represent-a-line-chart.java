1c402244d9f07d517975f5bdc27a2058180ebb02
Kriyanshi Shah <59494745+kriyanshishah@users.noreply.github.com>
2022-05-22 09:43:50 +0530

    Time: 69 ms (12.50%), Space: 103.8 MB (50.00%) - LeetHub

--- 2280-minimum-lines-to-represent-a-line-chart/2280-minimum-lines-to-represent-a-line-chart.java
+++ 2280-minimum-lines-to-represent-a-line-chart/2280-minimum-lines-to-represent-a-line-chart.java
@@ -1,14 +1,21 @@
 class Solution {
     public int minimumLines(int[][] stockPrices) {
-        int n = stockPrices.length;
-        if(n == 1) return 0;
-        Arrays.sort(stockPrices, (a,b)-> Integer.compare(a[0], b[0]));
-        int count = 1;
-        for(int i=2; i<n; i++){
-            // check (y2 - y1) / (x2 - x1) == (y1 - y0) / (x1 - x0)
-            if((stockPrices[i][1] - stockPrices[i-1][1]) *(stockPrices[i-1][0] - stockPrices[i-2][0]) == (stockPrices[i-1][1] - stockPrices[i-2][1]) * (stockPrices[i][0] - stockPrices[i-1][0])) continue;
-            count++;
+        Arrays.sort(stockPrices, (a, b) -> a[0] - b[0]);
+        
+        int result = Math.min(stockPrices.length - 1, 1);
+        long n1, n2, d1, d2;
+        
+        for (int i = 2; i < stockPrices.length; ++i ){
+            n1 = stockPrices[i][1] - stockPrices[i - 1][1];
+            d1 = stockPrices[i][0] - stockPrices[i - 1][0];
+            
+            n2 = stockPrices[i - 1][1] - stockPrices[i - 2][1];
+            d2 = stockPrices[i - 1][0] - stockPrices[i - 2][0];
+            
+            if (n1*d2 != n2*d1)
+                ++result;
         }
-        return count;
+        
+        return result;
     }
 }
