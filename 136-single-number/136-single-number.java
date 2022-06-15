15a79069b51d9c2260c34dcb38d9d19e838bd4d8
Kriyanshi Shah <59494745+kriyanshishah@users.noreply.github.com>
2022-06-15 23:59:12 +0530

    Time: 2 ms (59.46%), Space: 50.5 MB (53.54%) - LeetHub

--- 136-single-number/136-single-number.java
+++ 136-single-number/136-single-number.java
@@ -1,9 +1,9 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int mask = 0;
-        for(int i=0; i<nums.length;i++){
-            mask ^= nums[i];
+        int result = 0;
+        for(int i : nums){
+            result ^= i;
         }
-        return mask;
+        return result;
     }
 }
