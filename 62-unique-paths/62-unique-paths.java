b44fe45b55294887301d1b9760db3656d90d5f3c
Kriyanshi Shah <59494745+kriyanshishah@users.noreply.github.com>
2022-06-29 23:35:27 +0530

    Time: 1 ms (50.22%), Space: 40.9 MB (48.20%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -1,15 +1,19 @@
 class Solution {
-    public int uniquePaths(int m, int n) {
-        return uniquePath(m-1, n-1, new int[n][m]);
-    }
-    
-    public int uniquePath(int m ,int n, int[][] memory){
-        if(m < 0 || n < 0) return 0;
-        else if(m ==0 || n == 0) return 1;
-        else if(memory[n][m] > 0) return memory[n][m];
-        else{
-            memory[n][m] = uniquePath(m-1, n, memory) + uniquePath(m, n-1, memory);
-            return memory[n][m];
+    public int uniquePaths(int n, int m) {
+        int[][] grid = new int[m][n];
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+               if(i == 0){
+                   grid[i][j] = 1;
+               }
+               else if(j == 0) {
+                   grid[i][j] = 1;
+               }
+               else{
+                   grid[i][j] = grid[i - 1][j] + grid[i][j - 1];
+               } 
+            }
         }
+        return grid[m - 1][n - 1];
     }
 }
