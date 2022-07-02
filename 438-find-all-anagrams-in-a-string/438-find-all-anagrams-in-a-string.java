799bb6e12bc118e56fb85ff2101c1b88c4f2954d
Kriyanshi Shah <59494745+kriyanshishah@users.noreply.github.com>
2022-07-02 22:25:11 +0530

    Time: 16 ms (60.10%), Space: 47.8 MB (55.38%) - LeetHub

---
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.java
@@ -0,0 +1,42 @@
+class Solution {
+    public List<Integer> findAnagrams(String s, String p) {
+        List<Integer> list = new ArrayList<Integer>();
+        
+        if(p.length() > s.length()) return list;
+        int lenS = s.length();
+        int lenP = p.length();
+        
+        int[] count = freq(p);
+        int[] currCount = freq(s.substring(0, lenP));
+        
+        if(areSame(currCount, count)){
+            list.add(0);
+        }
+        
+        for(int i = lenP; i < lenS; i++){
+            currCount[s.charAt(i - lenP) - 'a']--;
+            currCount[s.charAt(i) - 'a']++;
+            if(areSame(currCount, count)){
+                list.add(i - lenP + 1);
+            }
+        }
+        return list;
+    }
+    
+    public boolean areSame(int[] x, int[] y){
+        for(int i = 0; i < 26; i++){
+            if(x[i] != y[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    public int[] freq(String s){
+        int[] count = new int[26];
+        for(char c: s.toCharArray()){
+            count[c - 'a']++;
+        }
+        return count;
+    }
+}
