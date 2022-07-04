bf7e3e450871120614d1a9b3b6ec2e418724af94
Kriyanshi Shah <59494745+kriyanshishah@users.noreply.github.com>
2022-07-04 10:26:39 +0530

    Time: 4 ms (70.03%), Space: 52.4 MB (18.04%) - LeetHub

--- 135-candy/135-candy.java
+++ 135-candy/135-candy.java
@@ -1,18 +1,20 @@
 class Solution {
     public int candy(int[] ratings) {
         int[] candies = new int[ratings.length];
-        Arrays.fill(candies, 1); // make sure that each kid gets 1 candy
+        Arrays.fill(candies, 1);
         for(int i = 1; i < ratings.length; i++){
-            if(ratings[i - 1] < ratings[i]) candies[i] = candies[i-1] + 1;
+            if(ratings[i] > ratings[i - 1]){
+                candies[i] = candies[i - 1] + 1;
+            }
         }
-        for(int i = ratings.length - 2; i >= 0; i--){
+        for(int i = candies.length - 2; i >= 0; i--){
             if(ratings[i] > ratings[i + 1]){
-                candies[i] = Math.max(candies[i + 1]  + 1, candies[i]);
+                candies[i] = Math.max(candies[i], candies[i + 1] + 1);
             }
         }
         int sum = 0;
-        for(int candy : candies){
-            sum += candy;
+        for(int c: candies){
+            sum += c;
         }
         return sum;
     }
