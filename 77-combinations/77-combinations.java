9dac177cdfdbf8903ebcc7d37f6a71a3dc96fe29
Kriyanshi Shah <59494745+kriyanshishah@users.noreply.github.com>
2022-07-06 16:19:59 +0530

    Time: 51 ms (23.49%), Space: 55 MB (40.46%) - LeetHub

---
+++ 77-combinations/77-combinations.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> list = new ArrayList<>();
+        comb(list, new ArrayList<>(), 1, n, k);
+        return list;
+    }
+    
+    public void comb(List<List<Integer>> list, List<Integer> tmp, int start, int n, int k){
+        if(k == 0){
+            list.add(new ArrayList<>(tmp));
+            return;
+        }
+        for(int i = start; i <= n; i++){
+            tmp.add(i);
+            comb(list, tmp, i + 1, n, k - 1);
+            tmp.remove(tmp.size() - 1);
+        }
+    }
+}
