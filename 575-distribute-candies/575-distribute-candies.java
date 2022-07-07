0c19dff6e2deb707e32175b84df96da3c9f61d9d
Kriyanshi Shah <59494745+kriyanshishah@users.noreply.github.com>
2022-07-07 20:47:00 +0530

    Time: 68 ms (36.42%), Space: 110.6 MB (27.64%) - LeetHub

---
+++ 575-distribute-candies/575-distribute-candies.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int distributeCandies(int[] candyType) {
+        Set<Integer> kinds = new HashSet<>();
+        for(int candy: candyType) kinds.add(candy);
+        return kinds.size() >= candyType.length / 2 ? candyType.length / 2 : kinds.size();
+    }
+}
