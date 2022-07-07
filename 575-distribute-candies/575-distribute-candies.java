ead47301793580cca044329f4703a5fc3772a0f6
Kriyanshi Shah <59494745+kriyanshishah@users.noreply.github.com>
2022-07-07 20:48:14 +0530

    Time: 9 ms (99.68%), Space: 79.8 MB (72.25%) - LeetHub

--- 575-distribute-candies/575-distribute-candies.java
+++ 575-distribute-candies/575-distribute-candies.java
@@ -1,7 +1,32 @@
 class Solution {
     public int distributeCandies(int[] candyType) {
-        Set<Integer> kinds = new HashSet<>();
-        for(int candy: candyType) kinds.add(candy);
-        return kinds.size() >= candyType.length / 2 ? candyType.length / 2 : kinds.size();
-    }
+		boolean[] map;
+		int nCandiesMax = 0;
+		int max = 0;
+		int min = 0;
+		int pos;
+		int nCandies = candyType.length;
+		
+		//Cerco il numero più piccolo e il più grande all'interno di CandyType
+		for(int candy : candyType) {
+			if(max < candy)
+				max = candy;
+			else if(min > candy)
+				min = candy;
+		}
+		
+		
+		map = new boolean[(max - min) + 1];
+		max = nCandies/2;
+		for(int candy : candyType) {
+			pos = candy - min;
+			if(!map[pos]) {
+				map[pos] = true;
+				if(++nCandiesMax >= max) {
+					return max;
+				}
+			}
+		}
+		
+		return nCandiesMax;    }
 }
