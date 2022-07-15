9dc8a12f6bcd9defcc6742bad6a9d29be91c50bf
Kriyanshi Shah <59494745+kriyanshishah@users.noreply.github.com>
2022-07-15 09:19:13 +0530

    Time: 3 ms (84.14%), Space: 47.8 MB (26.34%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.java
+++ 695-max-area-of-island/695-max-area-of-island.java
@@ -14,7 +14,7 @@ class Solution {
     public int areaOfIsland(int[][] grid, int i, int j){
         if(i >= 0 && i < grid.length && j >= 0 && j < grid[0].length && grid[i][j] == 1){
             grid[i][j] = 0;
-            return 1 + areaOfIsland(grid, i - 1, j) + areaOfIsland(grid, i + 1, j) + areaOfIsland(grid, i, j + 1) + areaOfIsland(grid, i, j - 1);
+            return 1 + areaOfIsland(grid, i - 1, j) + areaOfIsland(grid, i + 1, j) + areaOfIsland(grid, i, j - 1) + areaOfIsland(grid, i, j + 1);
         }
         return 0;
     }
