4ef81977b3aaa4a4c1bf6f1c87070939f65b1b6a
Kriyanshi Shah <59494745+kriyanshishah@users.noreply.github.com>
2022-07-20 13:51:43 +0530

    Time: 1 ms (99.55%), Space: 41.8 MB (84.62%) - LeetHub

--- 2062-count-vowel-substrings-of-a-string/2062-count-vowel-substrings-of-a-string.java
+++ 2062-count-vowel-substrings-of-a-string/2062-count-vowel-substrings-of-a-string.java
@@ -1,21 +1,32 @@
 class Solution {
     public int countVowelSubstrings(String word) {
-        int vow = 0;
-        Set<Character> set = new HashSet<>();
-        for(int i = 0; i < word.length() - 4; i++){
-            set.clear();
-            for(int j = i; j < word.length(); j++){
-                char ch = word.charAt(j);
-                if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'){
-                    set.add(ch);
-                    if(set.size() == 5){
-                        vow++;
-                    }
-                }else{
-                    break;
+        int start = 0, mid = 0, end = 0, res = 0, vowels = 0;
+        int[] chars = new int[26];
+        while (end < word.length()) {
+            if (isVowel(word.charAt(end))) {
+                chars[word.charAt(end) - 'a']++;
+                if (chars[word.charAt(end) - 'a'] == 1) {
+                    vowels++;
                 }
-            } 
+                while (vowels == 5) {
+                    chars[word.charAt(mid) - 'a']--;
+                    if (chars[word.charAt(mid) - 'a'] == 0) {
+                        vowels--;
+                    }
+                    mid++;
+                }
+                res += (mid - start);
+            } else {
+                start = end + 1;
+                mid = start;
+                vowels = 0;
+                chars = new int[26];
+            }
+            end++;
         }
-        return vow;
+        return res;
     }
+    private boolean isVowel(char c) {
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+    } 
 }
