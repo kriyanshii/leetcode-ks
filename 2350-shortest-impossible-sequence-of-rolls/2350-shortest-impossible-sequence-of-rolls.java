52536aa973a7c58a46088363c669ed75deeb8456
Kriyanshi Shah <59494745+kriyanshishah@users.noreply.github.com>
2022-07-25 17:30:41 +0530

    Time: 24 ms (80.00%), Space: 53 MB (100.00%) - LeetHub

--- 2350-shortest-impossible-sequence-of-rolls/2350-shortest-impossible-sequence-of-rolls.java
+++ 2350-shortest-impossible-sequence-of-rolls/2350-shortest-impossible-sequence-of-rolls.java
@@ -1,14 +1,15 @@
 class Solution {
     public int shortestSequence(int[] rolls, int k) {
-        int res = 1;
+        int n = rolls.length;
+        int res = 0;
         Set<Integer> set = new HashSet<>();
-        for(int roll: rolls){
-            set.add(roll);
-            if(set.size() == k){
-                res++;
+        for (int i = 0; i < n; i++) {
+            set.add(rolls[i]);
+            if (set.size() == k) {
                 set.clear();
+                res++;
             }
         }
-        return res;
+        return res + 1;
     }
 }
