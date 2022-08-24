110ba4395186cb5241de6c7297d75d8010b317a1
Kriyanshi Shah <59494745+kriyanshishah@users.noreply.github.com>
2022-08-24 18:36:56 +0530

    Time: 5 ms (20.27%), Space: 45.6 MB (77.19%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.java
+++ 64-minimum-path-sum/64-minimum-path-sum.java
@@ -7,9 +7,7 @@ class Solution {
                 if(i == 0 && j == 0) grid[i][j] = grid[i][j];
                 else if(i == 0 && j != 0) grid[i][j] += grid[i][j - 1];
                 else if(i != 0 && j == 0) grid[i][j] += grid[i - 1][j];
-                else{
-                    grid[i][j] = grid[i][j] + Math.min(grid[i][j - 1], grid[i - 1][j]);
-                }
+                else grid[i][j] += Math.min(grid[i][j - 1], grid[i - 1][j]);
             }
         }
         return grid[m - 1][n - 1];
