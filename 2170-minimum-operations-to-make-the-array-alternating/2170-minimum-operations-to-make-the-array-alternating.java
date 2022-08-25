cf2ff192e2caec58c14022e6795387be2c4da454
Kriyanshi Shah <59494745+kriyanshishah@users.noreply.github.com>
2022-08-25 14:38:00 +0530

    Time: 15 ms (99.32%), Space: 101.4 MB (81.51%) - LeetHub

--- 2170-minimum-operations-to-make-the-array-alternating/2170-minimum-operations-to-make-the-array-alternating.java
+++ 2170-minimum-operations-to-make-the-array-alternating/2170-minimum-operations-to-make-the-array-alternating.java
@@ -1,16 +1,35 @@
 class Solution {
     public int minimumOperations(int[] nums) {
-        int[][] freq = new int[100005][2];
-        int ans = 0;
-        for(int i = 0; i < nums.length; i++){
-            freq[nums[i]][i&1]++;
+ int maxOdd = 0,maxEven=0,max=0;
+        int n= nums.length;
+        for(int i=0;i<n;i++)max= Math.max(max,nums[i]);
+        int even[]=new int[max+1];
+        int odd[]=new int[max+1];
+        for(int i=0;i<n;i++){
+            if(i%2==0)even[nums[i]]++;
+            else odd[nums[i]]++;
         }
-        
-        for(int i = 0, j = 0, k = 0; i <= 100000; i++){
-            ans = Math.max(ans, Math.max(freq[i][0] + k, freq[i][1] + j));
-            j = Math.max(j, freq[i][0]);
-            k = Math.max(k, freq[i][1]);
+        int t1=0,t2=0;
+        for(int i=0;i<max+1;i++){
+            if(even[i]>maxEven){
+                maxEven=even[i];t1=i;
+            }
+            if(odd[i]>maxOdd){
+                maxOdd=odd[i];t2=i;
+            }
         }
-        return nums.length - ans;
+        int ans=0;
+        if(t1==t2){
+            int secondEven=0,secondOdd=0;
+            for(int i=0;i<max+1;i++){
+                if(i!=t1 && even[i]>secondEven)secondEven=even[i];
+                if(i!=t2 && odd[i]>secondOdd)secondOdd=odd[i];
+            }
+            ans = Math.min((n/2+n%2-maxEven)+ (n/2-secondOdd),(n/2+n%2-secondEven)+(n/2-maxOdd));
+        }
+        else{
+            ans = ((n/2+n%2)-maxEven)+ ((n/2)-maxOdd);  
+        } 
+        return ans;
     }
 }
