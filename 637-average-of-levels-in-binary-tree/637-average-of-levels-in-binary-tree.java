11778f3a6295193932aa47a060f540fecac0e2fe
Kriyanshi Shah <59494745+kriyanshishah@users.noreply.github.com>
2022-09-02 10:34:06 +0530

    Time: 3 ms (76.99%), Space: 48.4 MB (16.27%) - LeetHub

--- 637-average-of-levels-in-binary-tree/637-average-of-levels-in-binary-tree.java
+++ 637-average-of-levels-in-binary-tree/637-average-of-levels-in-binary-tree.java
@@ -22,10 +22,10 @@ class Solution {
             int count = q.size();
             double sum = 0;
             for(int i = 0; i < count; i++){
-                TreeNode curr = q.poll();
-                sum += curr.val;
-                if(curr.left != null) q.offer(curr.left);
-                if(curr.right != null) q.offer(curr.right);
+                TreeNode cur = q.remove();
+                sum += cur.val;
+                if(cur.left != null) q.offer(cur.left);
+                if(cur.right != null) q.offer(cur.right);
             }
             list.add(sum / count);
         }
