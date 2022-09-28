3f926f1b2c38bc8faac47ffcee583fc046554384
Kriyanshi Shah <59494745+kriyanshishah@users.noreply.github.com>
2022-09-28 19:08:50 +0530

    Time: 51 ms (31.13%), Space: 95.7 MB (68.59%) - LeetHub

--- 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.java
+++ 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.java
@@ -5,15 +5,15 @@ class Solution {
         // here if
         // sum + nums[i] >= max * size of array then
         // size of the array will be the max size.
-        int res = 1, i = 0;
+        int res = 1, j = 0;
         long sum = 0;
         Arrays.sort(nums);
-        for(int j = 0; j < nums.length; j++){
-            sum += nums[j];
-            while(sum + k < (long)nums[j] * (j - i + 1)){
-                sum -= nums[i++];
+        for(int i = 0; i < nums.length; i++){
+            sum += nums[i];
+            while(sum + k < (long) nums[i] *(i - j + 1)){
+                sum -= nums[j++];
             }
-            res = Math.max(res, j - i + 1);
+            res = Math.max(res, i - j + 1);
         }
         return res;
     }
