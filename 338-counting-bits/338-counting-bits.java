dc6fd0cbd91161f2a9e2caa02f2f0bae541d8acc
Kriyanshi Shah <59494745+kriyanshishah@users.noreply.github.com>
2022-10-03 22:32:59 +0530

    Time: 5 ms (39.75%), Space: 48 MB (65.12%) - LeetHub

--- 338-counting-bits/338-counting-bits.java
+++ 338-counting-bits/338-counting-bits.java
@@ -11,14 +11,13 @@ class Solution {
         if(n == 0) return 0;
         if(n == 1) return 1;
         if(memo[n] != 0) return memo[n];
-        
-        //if memo[n] does not exist then count
         if(n % 2 == 0){
             memo[n] = solve(n/2, memo);
-            return solve(n/2, memo);
+            // return solve(n/2, memo);
+            return memo[n];
         }else{
             memo[n] = 1 + solve(n/2, memo);
-            return 1 + solve(n/2, memo);
+            return memo[n];
         }
     }
 }
