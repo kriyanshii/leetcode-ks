3c93ef13277effa447f92ff94c7fe52f9496efd0
Kriyanshi Shah <59494745+kriyanshishah@users.noreply.github.com>
2022-11-01 14:04:09 +0530

    Time: 7 ms (34.60%), Space: 54.7 MB (18.80%) - LeetHub

---
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] findBall(int[][] grid) {
+        int m = grid.length, n = grid[0].length;
+        int[] res = new int[n];
+        for(int i = 0; i < n; i++){
+            int i1 = i, i2 = 0;
+            for(int j = 0; j < m; j++){
+                i2 = i1 + grid[j][i1];
+                if(i2 < 0 || i2 >= n || grid[j][i1] != grid[j][i2]){
+                    i1 = -1;
+                    break;
+                }
+                i1 = i2;
+            }
+            res[i] = i1;
+        }
+        return res;
+    }
+}
