942d3094a13e90276629c3c4d24a4d5919158283
Kriyanshi Shah <59494745+kriyanshishah@users.noreply.github.com>
2022-11-26 10:17:56 +0530

    Time: 138 ms (34.14%), Space: 66.6 MB (71.70%) - LeetHub

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.java
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.java
@@ -2,19 +2,18 @@ class Solution {
     public int sumSubarrayMins(int[] arr) {
         Stack<Integer> stack = new Stack<>();
         int[] dp = new int[arr.length + 1];
+        int res = 0, M = (int)1e9 + 7;
         stack.push(-1);
         
-        int result = 0, M = (int)1e9 + 7;
         for(int i = 0; i < arr.length; i++){
             while(stack.peek() != -1 && arr[i] <= arr[stack.peek()]){
                 stack.pop();
             }
-            dp[i+1] = (dp[stack.peek() + 1] + (i - stack.peek()) * arr[i]) % M;
+            dp[i + 1] = dp[stack.peek() + 1] + ((i - stack.peek()) * arr[i]) % M;
             stack.push(i);
-            result += dp[i + 1];
-            result %= M;
-            
+            res += dp[i + 1];
+            res %= M;
         }
-        return result;
+        return res;
     }
 }
