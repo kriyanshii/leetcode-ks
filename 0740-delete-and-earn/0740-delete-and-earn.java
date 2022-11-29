aa55f52890647f25b664eb8292c825bcf8e554b8
Kriyanshi Shah <59494745+kriyanshishah@users.noreply.github.com>
2022-11-30 00:00:32 +0530

    Time: 6 ms (86.93%), Space: 47.5 MB (52.43%) - LeetHub

---
+++ 0740-delete-and-earn/0740-delete-and-earn.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int deleteAndEarn(int[] nums) {
+        int n = 10001;
+        int[] values = new int[n];
+        for(int value: nums){
+            values[value] += value;
+        }
+        int take = 0, skip = 0;
+        for(int i = 0; i <values.length; i++){
+            int tmp = take;
+            take = skip + values[i];
+            skip = Math.max(tmp, skip);
+            // skip = skipi;
+            // take = takei;
+        }
+        return Math.max(take, skip);
+    }
+}
