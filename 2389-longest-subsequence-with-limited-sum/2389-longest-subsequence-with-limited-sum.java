00be1bfa52f03419c1b86d116cfa98d278c97843
Kriyanshi Shah <59494745+kriyanshishah@users.noreply.github.com>
2022-12-25 09:39:56 +0530

    Time: 4 ms (99.87%), Space: 42.7 MB (96.42%) - LeetHub

---
+++ 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] answerQueries(int[] nums, int[] queries) {
+        Arrays.sort(nums);
+        int n = nums.length, m = queries.length;
+        int[] res = new int[m];
+        for(int i = 1; i< n; i++){
+            nums[i] += nums[i - 1];
+        }
+        for(int i = 0; i < m; i++){
+            int ans = Arrays.binarySearch(nums,queries[i]);
+            res[i] = Math.abs(ans + 1);
+        }
+        return res;
+    }
+}
