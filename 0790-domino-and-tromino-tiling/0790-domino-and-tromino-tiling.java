33685872a8ce3e5ff720010206044d343d1f66d6
Kriyanshi Shah <59494745+kriyanshishah@users.noreply.github.com>
2022-12-25 17:00:27 +0530

    Time: 0 ms (100.00%), Space: 39.1 MB (96.59%) - LeetHub

--- 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.java
+++ 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.java
@@ -2,8 +2,8 @@ class Solution {
     private static final int mod = 1_000_000_007;
     public int numTilings(int n) {
         int p3 = -1, p2 = 0, p1 = 1;
-        for(int i = 1; i <= n; i++){
-            int cur = (int)((p1 * 2L + p3) % mod);
+        for(int i = 0; i < n; i++){
+            int cur = (int)((2L * p1 + p3) % mod);
             p3 = p2;
             p2 = p1;
             p1 = cur;
