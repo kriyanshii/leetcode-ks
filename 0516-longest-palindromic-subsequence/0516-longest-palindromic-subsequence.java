1824e95c8a20c3fbbada5799c46290ce067c62cc
Kriyanshi Shah <59494745+kriyanshishah@users.noreply.github.com>
2023-05-16 19:58:57 +0530

    Time: 61 ms (35.14%), Space: 51.7 MB (64.36%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
@@ -6,9 +6,9 @@ class Solution {
             dp[i][i] = 1;
             for(int j = i + 1; j < n; j++){
                 if(s.charAt(i) == s.charAt(j)){
-                    dp[i][j] = dp[i + 1][j - 1] + 2;
+                    dp[i][j] = 2 + dp[i + 1][j - 1];
                 }else{
-                    dp[i][j] = Math.max(dp[i + 1][j], dp[i][j-1]);
+                    dp[i][j] = Math.max(dp[i + 1][j], dp[i][j - 1]);
                 }
             }
         }
