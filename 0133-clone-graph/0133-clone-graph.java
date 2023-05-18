5377c893561c7e669a0143e060d7accc4138d027
Kriyanshi Shah <59494745+kriyanshishah@users.noreply.github.com>
2023-05-18 23:30:14 +0530

    Time: 25 ms (64.73%), Space: 42.2 MB (98.12%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.java
+++ 0133-clone-graph/0133-clone-graph.java
@@ -33,11 +33,17 @@ class Solution {
     }
     
     public Node cloneGraph(Node node) {
-        if(node == null) return null;
+        if(node == null) return node;
         Node copy = new Node(node.val);
         Node[] visited = new Node[101];
         Arrays.fill(visited, null);
         dfs(node, copy, visited);
         return copy;
+        //         if(node == null) return null;
+        // Node copy = new Node(node.val);
+        // Node[] visited = new Node[101];
+        // Arrays.fill(visited, null);
+        // dfs(node, copy, visited);
+        // return copy;
     }
 }
