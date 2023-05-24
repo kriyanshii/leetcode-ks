df814d487f9e306274f7390a64fc75835ba07513
Kriyanshi Shah <59494745+kriyanshishah@users.noreply.github.com>
2023-05-24 21:02:38 +0530

    Time: 106 ms (32.76%), Space: 60.6 MB (45.98%) - LeetHub

--- 2542-maximum-subsequence-score/2542-maximum-subsequence-score.java
+++ 2542-maximum-subsequence-score/2542-maximum-subsequence-score.java
@@ -1,19 +1,18 @@
 class Solution {
-    public long maxScore(int[] nums1, int[] nums2, int k) {
-        int n = nums1.length;
-        int[][] res = new int[n][2];
-        for(int i = 0; i < nums1.length; i++){
-            res[i] = new int[]{nums2[i], nums1[i]};
-        }
-        Arrays.sort(res, (a,b) -> (b[0] - a[0]));
+    public long maxScore(int[] speed, int[] efficiency, int k) {
+        int n = speed.length;
+        int[][] ess = new int[n][2];
+        for (int i = 0; i < n; ++i)
+            ess[i] = new int[] {efficiency[i], speed[i]};
+        Arrays.sort(ess, (a, b) -> b[0] - a[0]);
         PriorityQueue<Integer> pq = new PriorityQueue<>(k, (a, b) -> a - b);
-        long sum = 0, ans = 0;
-        for(int[] r: res){
-            pq.offer(r[1]);
-            sum += r[1];
-            if(pq.size() > k) sum -= pq.poll();
-            if(pq.size() == k) ans = Math.max(ans, sum * r[0]);
+        long res = 0, sumS = 0;
+        for (int[] es : ess) {
+            pq.add(es[1]);
+            sumS = (sumS + es[1]);
+            if (pq.size() > k) sumS -= pq.poll();
+            if (pq.size() == k) res = Math.max(res, (sumS * es[0]));
         }
-        return ans;
+        return res;
     }
 }
