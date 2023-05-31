c4ae8c8af036f9eecc9bc3c73f22f2824795d2d7
Kriyanshi Shah <59494745+kriyanshishah@users.noreply.github.com>
2023-05-31 22:36:59 +0530

    Time: 102 ms (63.72%), Space: 55.3 MB (67.30%) - LeetHub

--- 1396-design-underground-system/1396-design-underground-system.java
+++ 1396-design-underground-system/1396-design-underground-system.java
@@ -1,48 +1,74 @@
+class Passenger{
+    int checkinTime;
+    int checkoutTime;
+    String checkinLocation;
+    String checkoutLocation;
+
+    public Passenger(String checkinLocation, int checkinTime) {
+        this.checkinLocation = checkinLocation;
+        this.checkinTime = checkinTime;
+    }
+
+    void checkout(String checkoutLocation, int checkoutTime) {
+        this.checkoutLocation = checkoutLocation;
+        this.checkoutTime = checkoutTime;
+    }
+}
+
+class Route{
+    String startStation;
+    String endStation;
+    int totalNumberOfTrips;
+    double totalTimeSpentInTrips;
+    
+    public Route(String startStation, String endStation) {
+        this.startStation = startStation;
+        this.endStation = endStation;
+    }
+
+    double getAverageTime() {
+        return (double) totalTimeSpentInTrips / totalNumberOfTrips;
+    }
+
+    void addTrip(int startTime, int endTime) {
+        totalTimeSpentInTrips += endTime - startTime;
+        totalNumberOfTrips++;
+    }
+
+}
+
 class UndergroundSystem {
 
-    Map<Integer, ArrivalInfo> arrivals;
-    Map<String , double[]> total;
-    
-    public UndergroundSystem(){
-        arrivals = new HashMap<>();
-        total = new HashMap<>();
-    }
-    
-    public void checkIn(int id, String stationName, int t) {
-        arrivals.put(id, new ArrivalInfo(id, stationName, t));
-    }
-    
-    public void checkOut(int id, String stationName, int t) {
-        ArrivalInfo arrival = arrivals.get(id);
-        String key = arrival.stationName + "_" + stationName;
-        double[] pair = total.getOrDefault(key, new double[2]);
-        int time = t - arrival.time;
-        pair[0] += time;
-        // System.out.println(pair[0]);
-        pair[1]++;
-        
-        System.out.println(key);
-        total.put(key, pair);        
-    }
-    
-    public double getAverageTime(String startStation, String endStation) {
-        String key = startStation + "_" + endStation;
-        // System.out.println(key);
 
-        double[] pair = total.get(key);
-        // System.out.println(total.get(key));
-        return pair[0]/pair[1];
+    Map<Integer, Passenger> currentPassengerMap;
+    Map<String, Route> routeMap;
+
+    public UndergroundSystem() {
+        currentPassengerMap = new HashMap<>();
+        routeMap = new HashMap<>();
     }
-    
-    class ArrivalInfo{
-        int id;
-        String stationName;
-        int time;
-        ArrivalInfo(int id, String stationName, int time){
-            this.id = id;
-            this.stationName = stationName;
-            this.time = time;
+
+    public void checkIn(int id, String stationName, int t) {
+        if (!currentPassengerMap.containsKey(id)) {
+            Passenger passenger = new Passenger(stationName, t);
+            currentPassengerMap.put(id, passenger);
         }
+    }
+
+    public void checkOut(int id, String stationName, int t) {
+        if (currentPassengerMap.containsKey(id)) {
+            Passenger passenger = currentPassengerMap.get(id);
+            passenger.checkout(stationName, t);
+            String routeKey = passenger.checkinLocation + "," + passenger.checkoutLocation;
+            Route route = routeMap.getOrDefault(routeKey, new Route(passenger.checkinLocation, passenger.checkoutLocation));
+            route.addTrip(passenger.checkinTime, passenger.checkoutTime);
+            routeMap.put(routeKey, route);
+            currentPassengerMap.remove(id);
+        }
+    }
+
+    public double getAverageTime(String startStation, String endStation) {
+        return routeMap.get(startStation + "," + endStation).getAverageTime();
     }
 }
 
