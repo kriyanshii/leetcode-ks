d9ca29a7d369b52a1b51be50693b18269a21537c
Kriyanshi Shah <59494745+kriyanshishah@users.noreply.github.com>
2023-06-06 05:48:23 +0530

    Time: 130 ms (20.66%), Space: 64.5 MB (26.17%) - LeetHub

--- 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.java
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.java
@@ -1,22 +1,24 @@
 class Solution {
-    public int numOfMinutes(final int n, final int headID, final int[] manager, final int[] informTime) {
-        final Map<Integer, List<Integer>> graph = new HashMap<>();
-        int total = 0;
-        for (int i = 0; i < manager.length; i++) {
+    public int numOfMinutes(int n, int headID, int[] manager, int[] informTime) {
+        Map<Integer, List<Integer>> graph = new HashMap<>();
+        for(int i = 0; i < manager.length; i++){
             int j = manager[i];
-            if (!graph.containsKey(j))
+            if(!graph.containsKey(j)){
                 graph.put(j, new ArrayList<>());
+            }
             graph.get(j).add(i);
         }
         return dfs(graph, informTime, headID);
     }
-
-    private int dfs(final Map<Integer, List<Integer>> graph, final int[] informTime, final int cur) {
+    
+    public int dfs(Map<Integer, List<Integer>> graph, int[] informationTime, int cur){
         int max = 0;
-        if (!graph.containsKey(cur))
+        if(!graph.containsKey(cur)){
             return max;
-        for (int i = 0; i < graph.get(cur).size(); i++)
-            max = Math.max(max, dfs(graph, informTime, graph.get(cur).get(i)));
-        return max + informTime[cur];
+        }
+        for(int i = 0; i < graph.get(cur).size(); i++){
+            max = Math.max(max, dfs(graph, informationTime, graph.get(cur).get(i)));
+        }
+        return max + informationTime[cur];
     }
 }
